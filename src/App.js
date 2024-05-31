533454a6d148584d209965cfea1b5accae4116f7
MSoleimanzadeh <mina.soleimanzadeh@gmail.com>
2024-06-01 00:00:14 +0330

    Add reset function

--- src/App.js
+++ src/App.js
@@ -77,29 +77,35 @@ function App() {
       setXPoint(points => [...points, selectedPoint]);
       selectedBtn.innerText = 'X';
     }
-    setIsOsTurn(turn => !turn);
+  }
+
+  function resetGame() {
+    setIsOsTurn(true);
+    setOPoint([]);
+    setXPoint([]);
+    setWinner(null);
+    document.querySelectorAll('.point-btn').forEach(btn => btn.innerText = '');
   }
 
   useEffect(() => {
-    if (oPoint.length >= 3 && checkIfWon(oPoint))
+    if (isOsTurn && oPoint.length >= 3 && checkIfWon(oPoint))
       setWinner('O');
-  }, [oPoint]);
-
-  useEffect(() => {
-    if (xPoint.length >= 3 && checkIfWon(xPoint))
+    else if (!isOsTurn && xPoint.length >= 3 && checkIfWon(xPoint))
       setWinner('X');
-  }, [xPoint])
+    else if (oPoint.length + xPoint.length === 9)
+      setWinner('Nobody');
+    setIsOsTurn(turn => !turn);
+  }, [xPoint, oPoint])
 
   return (
-    <div className='pt-20'>
-      {!!winner && (<h1 className='text-center text-[22px]'>The winner is <span className='font-bold'>{winner}</span></h1>)}
+    <div className='pt-28'>
+      {!!winner && (<h1 className='text-center text-[22px]'><span className='font-bold'>{winner}</span> won</h1>)}
       {!winner && <h2 className='text-center text-[20px]'><span className='font-bold'>{isOsTurn ? 'O' : 'X'}</span>'s turn</h2>}
       <div className='grid grid-cols-3 w-36 mx-auto mt-20'>
-        {allPoints.map(point => <button key={point.index} id={point.index} onClick={() => submitChoice(point)} className='point-btn w-12 h-12 bg-yellow-400 hover:bg-yellow-500'></button>)}
+        {allPoints.map(point => <button disabled={!!winner} key={point.index} id={point.index} onClick={() => submitChoice(point)} className='point-btn w-12 h-12 bg-yellow-400 hover:bg-yellow-500'></button>)}
       </div>
-      {!!winner && <button className='play-btn block bg-cyan-500 text-white'>Play again!</button>}
-      {!winner && <button className='play-btn block hover:bg-cyan-500 text-cyan-700 hover:text-white border border-cyan-500 hover:border-transparent'>Retry</button>}
-
+      {!!winner && <button onClick={resetGame} className='play-btn block bg-cyan-500 text-white'>Play again!</button>}
+      {!winner && <button onClick={resetGame} className='play-btn block bg-white hover:bg-cyan-500 text-cyan-700 hover:text-white border border-cyan-500 hover:border-transparent'>Retry</button>}
     </div>
   );
 }
